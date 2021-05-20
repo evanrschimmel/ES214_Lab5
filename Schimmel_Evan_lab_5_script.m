c09b97899f6be6ad4fd14af71517ab71954d0ee0
Evan Schimmel <evanrschimmel@gmail.com>
2021-05-19 21:05:18 -0400

    Definition comments added
    For constants and initial conditions

--- Schimmel_Evan_lab_5_script.m
+++ Schimmel_Evan_lab_5_script.m
@@ -4,20 +4,20 @@ close all
 
 load('working_model_results');
 
-m_a=1.0; %kg
-m_d=0.16; %kg
-L=1.0; %m
-R=0.133; %m
-Ja=0.75; %kg-m^2
-Jd=0.0014151; %kg-m^2
-k=10.0; %N-m/rad
+m_a=1.0; %kg, mass of bar
+m_d=0.16; %kg, mass of disk
+L=1.0; %m, length of bar
+R=0.133; %m, distance from rotation point to disk center
+Ja=0.75; %kg-m^2, mass moment of inertia of bar
+Jd=0.0014151; %kg-m^2, mass moment of inertia of disk
+k=10.0; %N-m/rad, spring constant
 
-thetaA0=0; %rad
-theta_dot_A0=0; %rad/s
-thetaD0=0; %rad
-thetadot_D0=0; %rad/s
-r0=0.2; %m
-tf=0.414;
+thetaA0=0; %rad, initial bar displacement
+theta_dot_A0=0; %rad/s, initial bar angular velocity
+thetaD0=0; %rad, initial disk displacement
+thetadot_D0=0; %rad/s, initial disk angular velocity
+r0=0.2; %m, initial distance from rotation point to disk center
+tf=0.414; %s, simulation runtime
 maxstep=0.01;
 tol=1e-6;
 
